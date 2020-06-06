e7c867678a41038ce6eddbf83e0a77b23bbfdbff
Qingyu Li <ga94bur@mytum.de>
2020-06-06 23:47:04 +0200

    upload backend service for edit customer profile

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const customerModel = require('../models/customer');
 const tutorModel = require('../models/tutor');
+const customerModel = require('../models/customer');
 const config = require('../config');
 
 
@@ -256,10 +257,82 @@ const uploadTutorProfile = (req, res) => {
     }
 
 }
+const getCustomerProfile = (req, res) => {
+    if (!Object.prototype.hasOwnProperty.call(req.body, 'email')) return res.status(400).json({
+        error: 'Bad Request',
+        message: 'The request body must contain a email property'
+    });
 
+    if (!Object.prototype.hasOwnProperty.call(req.body, 'userType')) return res.status(400).json({
+        error: 'Bad Request',
+        message: 'The request body must contain a userType property'
+    });
+
+    if (req.body.userType === 'customer') {
+        customerModel.findOne({email: req.body.email}).exec()
+        .then(customer => {
+            return res.status(200).json({
+                email: customer.email,
+                firstName: customer.firstName,
+                lastName: customer.lastName,
+                university: customer.university,
+            })
+        })
+    }
+}
+
+const uploadCustomerProfile = (req, res) => {
+    if (!Object.prototype.hasOwnProperty.call(req.body, 'email')) return res.status(400).json({
+        error: 'Bad Request',
+        message: 'The request body must contain a email property'
+    });
+    if (req.body.userType === 'customer') {
+        if (!Object.prototype.hasOwnProperty.call(req.body, 'firstName')) return res.status(400).json({
+            error: 'Bad Request',
+            message: 'The request body must contain a firstName property'
+        });
+    
+        if (!Object.prototype.hasOwnProperty.call(req.body, 'lastName')) return res.status(400).json({
+            error: 'Bad Request',
+            message: 'The request body must contain a lastName property'
+        });
+    
+        if (!Object.prototype.hasOwnProperty.call(req.body, 'university')) return res.status(400).json({
+            error: 'Bad Request',
+            message: 'The request body must contain a university property'
+        });
+    
+        const customer = Object.assign({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            university: customer.university,
+        });
+        customerModel.updateOne({email: customer.email}, customer).then(customer => {
+            return res.status(200).json({message: "successfully updated"});
+        }).catch(error => {
+            console.log('error by creating a customer Profile');
+            if (error.code == 11000) {
+                return res.status(400).json({
+                    error: 'customer Profile exists',
+                    message: error.message
+                })
+            }
+            else {
+                return res.status(500).json({
+                    error: 'Internal server error happens by add customer Profile',
+                    message: error.message
+                })
+            }
+        });
+    }
+
+}
 module.exports = {
     login,
     register,
     getTutorProfile,
     uploadTutorProfile,
+    getCustomerProfile,
+    uploadCustomerProfile,
 };
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -5,5 +5,7 @@ const userController = require('../controllers/user');
 router.post('/login', userController.login);
 router.post('/register', userController.register);
 router.post('/uploadTutorProfile', userController.uploadTutorProfile);
+router.post('/uploadCustomerProfile', userController.uploadCustomerProfile);
 router.get('/tutorProfile', userController.getTutorProfile);
+router.get('/customerProfile', userController.getCustomerProfile);
 module.exports = router;
