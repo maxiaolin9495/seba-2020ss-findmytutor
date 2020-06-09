1d356c95343574c939c592f1220d1a2bceb14ea9
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2020-06-09 20:16:05 +0200

    revoke value of mongodb db

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,7 +2,7 @@
 
 //Configuration variables
 const port      = process.env.PORT        || '3000';
-const mongoURI  = process.env.MONGODB_URI || 'mongodb://192.168.99.100:27017/mongodb';
+const mongoURI  = process.env.MONGODB_URI || 'mongodb://localhost:27017/mongodb';
 const JwtSecret = process.env.JWT_SECRET  || 'very secret secret';
 
 module.exports = {
