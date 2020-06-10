e3a59974db1099970c6ec636cac223ae7d8f665b
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2020-06-10 22:41:28 +0200

    switch from emailjs to nodemailer

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,43 +1,65 @@
-import * as emailjs from "emailjs-com";
+const nodemailer = require('nodemailer');
 
-const serviceId = "gmail";
-const feedbackTemplateId = "feedbackFindMyTutor";
-const notificationTemplateId = 'Notification';
-const userId = 'user_dSKdVGR3vH7TctvEXGiI7';
 const newTutorial = 'You have new Tutorial Session from FindMyTutor, please confirm it or cancel it in soon.';
 const confirmTutorial = 'Your Tutorial Session  has just been confirmed by tutor ';
 const cancelTutorial = 'Your Tutorial Session  has just been canceled';
 const reviewTutorial = 'Your Tutorial Session  has been just reviewed';
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'findmytutor2020@gmail.com',
+        pass: 'Zxcv12345'
+    }
+});
+
 //email endpoint for contact us
 const contactUs = (email, firstName) => {
-    return new Promise((resolve, reject) => {
-        emailjs.send(serviceId, feedbackTemplateId, {
-            "to_email": email,
-            "to_name": firstName
-        }, userId).then (function (response) {
-            resolve(response)
-        }, function (err) {
-            console.log(err);
-            reject(err)
-        });
-    })
+
+    let mailOptions = {
+        from: 'findmytutor2020@gmail.com',
+        to: email,
+        subject: 'New message from FindMyTutor',
+        text: 'Hello '+ firstName +',\n' +
+            '\n' +
+            'we have received your request. Our customer service team will contact you as soon as possible. \n' +
+            '\n' +
+            ' \n' +
+            'Best Regards\n' +
+            'Your FindMyTutor Team'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
 };
 
 const emailNotification = (email, firstName, subject, message) =>{
-    return new Promise((resolve, reject) => {
-        emailjs.send(serviceId, notificationTemplateId, {
-            "to_email": email,
-            "to_name": firstName,
-            "subject": subject,
-            "message": message
-        }, this.userId).then(function (response) {
-            resolve(response)
-        }, function (err) {
-            console.log(err);
-            reject(err)
-        });
-    })
+
+    let mailOptions = {
+        from: 'findmytutor2020@gmail.com',
+        to: email,
+        subject: subject,
+        text: 'Hello '+ firstName +',\n' +
+            '\n' +
+            message +
+            '\n' +
+            'Best Regards\n' +
+            'Your FindMyTutor Team'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 };
 
 module.exports = {
