b564f1cfe3bfc2593d97a2e6ec79f68113d085a9
Qingyu Li <ga94bur@mytum.de>
2020-06-17 01:53:08 +0200

    implement homepage

---
+++ frontend/src/ViewController/SearchResultView.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import SearchResultPage from '../UIcomponents/Search/SearchResultPage';
+import Background from '../Images/searchresultbg.jpg';
+import Navigation from '../UIcomponents/PageDesign/Navigation.js';
