b605202e9d7d6c0ba28ec5d14fb0ec4c3f012514
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2020-06-16 12:55:29 +0200

    update http methods for create review and booking endpoints

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -5,7 +5,7 @@ const middleWares = require('../middleWares');
 
 router.post('/login', userController.login);
 router.post('/register', userController.register);
-router.put('/createTutorial', middleWares.checkAuthentication, userController.createTutorial);
+router.post('/createTutorial', middleWares.checkAuthentication, userController.createTutorial);
 router.post('/cancelTutorial', middleWares.checkAuthentication, userController.cancelTutorial);
 router.post('/closeTutorial', middleWares.checkAuthentication, userController.closeTutorial);
 router.post('/getAllTutorials', middleWares.checkAuthentication, userController.getAllTutorials);
