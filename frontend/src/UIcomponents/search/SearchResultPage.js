aaacc8499ef7822eabe65d9b1996e088ef85af55
Qingyu Li <ga94bur@mytum.de>
2020-06-18 14:18:16 +0200

    add university filter

--- frontend/src/UIcomponents/search/SearchResultPage.js
+++ frontend/src/UIcomponents/search/SearchResultPage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 import SearchResultCard from "./SearchResultCard";
+import { MultiSelect } from '@progress/kendo-react-dropdowns';
+import '@progress/kendo-theme-default/dist/all.css';
+
 // export const history = createHashHistory();
 
 // const universities = {
@@ -11,21 +14,38 @@ import SearchResultCard from "./SearchResultCard";
 //     Price: []
 // }
 
-
 class SearchResultPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards:[]
+            cards: [],
+            value:[],
+            universities: []
         }
     }
+    onChange = (event) => {
+        this.setState({
+            value: [ ...event.target.value ]
+        });
+    }
+
+
+
     componentWillReceiveProps(props) {
         this.setState({
             cards: props.filteredData.map(d => {
                 console.log(d);
-                return(<SearchResultCard
+                return (<SearchResultCard
                     key={d.id}
                     tutor={d} />)
+            })
+        })
+        let altUniversities = props.filteredData.map(function(uni) {
+            return uni.university
+          })
+        this.setState({
+            universities: altUniversities.filter(function(item, pos) {
+                return altUniversities.indexOf(item) == pos;
             })
         })
     }
@@ -39,10 +59,39 @@ class SearchResultPage extends Component {
     };
 
     render() {
-        return (<div>
-            {this.state.cards}
-        </div>);
+
+        return (
+            <div style={{ marginBottom: "40px" }}>
+                <div className="md-block-centered" style={{
+                    flexDirection: 'row',
+                    width: '70%',
+                    height: '300px',
+                    background: 'white',
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                    padding: '20px',
+                    paddingBottom: '20px'
+                }}>
+                     <div className="example-wrapper">
+                <div>
+                    <div>University:</div>
+                    <MultiSelect
+                        data={this.state.universities}
+                        onChange={this.onChange}
+                        value={this.state.value}
+                    />
+                </div>
+            </div>
+                </div>
+                
+                <div style={{
+                    position: 'relative',
+                }}>
+                    {this.state.cards}
+                </div>
+            </div>
+        );
     }
 }
 
 export default withRouter(SearchResultPage);
+
