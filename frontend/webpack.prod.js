f918f47d65f70b956d530ea2a5406c5567b97335
ga26piq <ga26piq@mytum.de>
2020-07-09 14:24:23 +0200

    For production

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,18 +1,23 @@
 "use strict";
 
-const webpack        = require('webpack');
-const merge          = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // not es6 compatible (conflict with socket.io)
+const TerserPlugin = require('terser-webpack-plugin');
 
-const common         = require('./webpack.common.js');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
     plugins: [
-        new UglifyJSPlugin({
-            output: {
-                comments: false,
-            },
-        }),
+        // new UglifyJSPlugin({
+        //     output: {
+        //         comments: false,
+        //     },
+        // }),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
