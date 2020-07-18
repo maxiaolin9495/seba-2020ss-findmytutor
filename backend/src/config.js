646cb480c8939ca08fa57e1380fa57987d2d952e
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2020-07-18 22:59:02 +0200

    change config environmental injections' names

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,8 +4,8 @@
 const port      = process.env.PORT        || '3000';
 const mongoURI  = process.env.MONGODB_URI || 'mongodb://localhost:27017/mongodb';
 const JwtSecret = process.env.JWT_SECRET  || 'very secret secret';
-const gmailAccount = process.env.gmailAccount || 'findmytutor2020@gmail.com';
-const gmailPassword = process.env.gmailPassword || 'Zxcv12345';
+const gmailAccount = process.env.GMAIL_ACCOUNT || 'findmytutor2020@gmail.com';
+const gmailPassword = process.env.GMAIL_PASSWORD || 'Zxcv12345';
 
 module.exports = {
     port,
